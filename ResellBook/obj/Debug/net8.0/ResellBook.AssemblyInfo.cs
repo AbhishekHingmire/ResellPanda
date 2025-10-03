ee52d37dbd07b13b6dac91bbe825d0d47c44a407
ABHISHEK.HINGMIRE <ABHISHEK.HINGMIRE@wolterskluwer.com>
2025-10-03 23:50:35 +0530

    Revert "changes according to listing"
    This reverts commit b02252bfd44ccf360b47fa6efcd1a666e2526fef.

--- ResellBook/obj/Debug/net8.0/ResellBook.AssemblyInfo.cs
+++ ResellBook/obj/Debug/net8.0/ResellBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51c608b197368d72864ac1e31597a704d881ad44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a021ce744dba45d8aee2b9e8f48117b676629cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
