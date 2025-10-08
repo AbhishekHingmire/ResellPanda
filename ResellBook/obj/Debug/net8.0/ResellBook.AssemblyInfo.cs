8bd97bb44182ec851e6a92a6b59a2881460aedd3
ranas22 <120771065+ranas22@users.noreply.github.com>
2025-10-08 10:08:38 +0530

    edit

--- ResellBook/obj/Debug/net8.0/ResellBook.AssemblyInfo.cs
+++ ResellBook/obj/Debug/net8.0/ResellBook.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abe4ba1684af957d797f24496a89f054504e33b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8b1c90ce6d3ba558d83cda9b4f6b809b34c449")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResellBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
